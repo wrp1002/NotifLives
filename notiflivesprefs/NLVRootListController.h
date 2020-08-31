7202fdf1bc1124707a703859624a4e5014daf4cc
wrp1002 <wes.hamster@gmail.com>
2020-08-30 20:58:34 -0400

    Preferences fancier

--- notiflivesprefs/NLVRootListController.h
+++ notiflivesprefs/NLVRootListController.h
@@ -7,18 +7,10 @@
 #import <AppList/AppList.h>
 #import <SparkAppList/SparkAppListTableViewController.h>
 
-@interface JBBulletinManager : NSObject
-	+(id)sharedInstance;
-	-(id)showBulletinWithTitle:(NSString *)title message:(NSString *)msg bundleID:(NSString *)bundleID;
-	-(id)showBulletinWithTitle:(NSString *)title message:(NSString *)msg bundleID:(NSString *)bundleID soundPath:(NSString *)soundPath;
-	-(id)showBulletinWithTitle:(NSString *)title message:(NSString *)msg bundleID:(NSString *)bundleID soundID:(int)inSoundID;
-	-(id)showBulletinWithTitle:(NSString *)title message:(NSString *)msg overrideBundleImage:(UIImage *)bundleImage;
-	-(id)showBulletinWithTitle:(NSString *)title message:(NSString *)msg overrideBundleImage:(UIImage *)bundleImage soundPath:(NSString *)inSoundPath;
-	-(id)showBulletinWithTitle:(NSString *)title message:(NSString *)msg inOverridBundleImage:(UIImage *)bundleImage soundID:(int)soundID;
-	-(id)showBulletinWithTitle:(NSString *)title message:(NSString *)msg bundleID:(NSString *)bundleID hasSound:(BOOL)hasSound soundID:(int)soundID vibrateMode:(int)vibrate soundPath:(NSString *)soundPath attachmentImage:(UIImage *)attachmentImage overrideBundleImage:(UIImage *)overrideBundleImage;
+@interface PSListController (iOS12Plus)
+-(BOOL)containsSpecifier:(PSSpecifier *)arg1;
 @end
 
 @interface NLVRootListController : PSListController
-	
-	
+	@property (nonatomic, retain) NSMutableDictionary *savedSpecifiers;
 @end
--- notiflivesprefs/NLVRootListController.m
+++ notiflivesprefs/NLVRootListController.m
@@ -7,8 +7,53 @@
 		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
 	}
 
+	//In this array you should add the IDs of all the specifiers you are going to hide & show.
+	//Do not include the IDs of the cells you will reinsert them under.
+	//Notice I only included "cellID" and not "switchID".
+	NSArray *chosenIDs = @[@"kSelectButton"];
+	self.savedSpecifiers = (self.savedSpecifiers) ?: [[NSMutableDictionary alloc] init];
+	for(PSSpecifier *specifier in _specifiers) {
+		if([chosenIDs containsObject:[specifier propertyForKey:@"id"]]) {
+			[self.savedSpecifiers setObject:specifier forKey:[specifier propertyForKey:@"id"]];
+		}
+	}
+
 	return _specifiers;
 }
+
+-(void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier {
+	[super setPreferenceValue:value specifier:specifier];
+
+	//Here we will check if the value of our switch has been set to.
+	//We then check if the value has been set to NO in the following if statement to remove the other cell.
+	//Otherwise we check if the cell currently exists in our preferences, if not we reinserting the cell after the switch cell using its ID.
+	NSString *key = [specifier propertyForKey:@"key"];
+	if([key isEqualToString:@"kAllEnabled"]) {
+		if([value boolValue])
+			[self removeContiguousSpecifiers:@[self.savedSpecifiers[@"kSelectButton"]] animated:YES];
+		else if(![self containsSpecifier:self.savedSpecifiers[@"kSelectButton"]])
+			[self insertContiguousSpecifiers:@[self.savedSpecifiers[@"kSelectButton"]] afterSpecifierID:@"kAllEnabled" animated:YES];
+	}
+}
+
+-(void)reloadSpecifiers {
+  [super reloadSpecifiers];
+
+  //Since we don't have access to a specific specifier and value like in the previous step, we just have to read our preferences file.
+  //I check if our switchKey is NO, then hide the specifier
+  //Customize this to however you get your preferences, whether is directly from a plist or Cephei.
+  HBPreferences *preferences = [[HBPreferences alloc] initWithIdentifier:@"com.wrp1002.notiflives"];
+  if([preferences boolForKey:@"kAllEnabled"]) {
+    [self removeContiguousSpecifiers:@[self.savedSpecifiers[@"kSelectButton"]] animated:YES];
+  }
+}
+
+-(void)viewDidLoad {
+  [super viewDidLoad];
+  [self reloadSpecifiers];
+}
+
+
 
 -(void)Respring {
     pid_t pid;
